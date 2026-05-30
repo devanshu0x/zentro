ccbc1628bf959120cdd05618f49cfab41183c72e
devanshu0x <devanshudotdev@gmail.com>
2026-05-30 16:51:24 +0530

    feat: parse media header

--- parser/reader.go
+++ parser/reader.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"log"
 	"os"
-	"strings"
 )
 
 func ReadBlock(file io.Reader, b interface{}) error {
@@ -14,15 +13,9 @@ func ReadBlock(file io.Reader, b interface{}) error {
 	return err
 }
 
-func printBoxHeader(h *BoxHeader, level int) {
-	prefeix := strings.Repeat("\t", level)
-	fmt.Printf("%sBlock type: %s\n", prefeix, h.BoxType())
-	fmt.Printf("%sBlock size: %d\n", prefeix, h.Size)
-
-}
-
-func isContainer(headerType string) bool {
-	if headerType == "moov" || headerType == "trak" || headerType == "mdia" {
+func isContainer(boxType string) bool {
+	switch boxType {
+	case "moov", "trak", "mdia", "minf", "stbl":
 		return true
 	}
 	return false
@@ -53,7 +46,12 @@ func ReadHeaderDataVersioned(file io.ReadSeeker, end int64, v0 Printable, v1 Pri
 }
 
 func ReadHeaderData(file io.ReadSeeker, end int64, h Printable) error {
-	err:=binary.Read(file,binary.BigEndian,h)
+	fullBox := FullBox{}
+	err := binary.Read(file, binary.BigEndian, &fullBox)
+	if err != nil {
+		return fmt.Errorf("Failed to read fullbox: %w", err)
+	}
+	err=binary.Read(file,binary.BigEndian,h)
 	if err!=nil{
 		return err
 	}
@@ -94,6 +92,10 @@ func ParseBoxes(file io.ReadSeeker, end int64, level int) error {
 				ReadHeaderDataVersioned(file, curr+int64(header.Size),&MovieHeaderv0{},&MovieHeaderv1{})
 			case "tkhd":
 				ReadHeaderDataVersioned(file, curr+int64(header.Size),&TrackHeaderv0{},&TrackHeaderv1{})
+            case "hdlr":
+				ReadHeaderData(file,curr+int64(header.Size),&HandlerReference{})
+			case "mdhd":
+				ReadHeaderDataVersioned(file, curr+int64(header.Size), &MediaHeaderv0{},&MediaHeaderv1{})	
 			default:
 				_, err := file.Seek(int64(header.Size)-8, io.SeekCurrent)
 				if err != nil {
--- parser/types.go
+++ parser/types.go
@@ -1,15 +1,24 @@
 package parser
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-
-type Printable interface{
+type Printable interface {
 	Print()
 }
 
 type BoxHeader struct {
 	Size uint32
 	Type [4]byte
+}
+
+func printBoxHeader(h *BoxHeader, level int) {
+	prefeix := strings.Repeat("\t", level)
+	fmt.Printf("%sBlock type: %s\n", prefeix, h.BoxType())
+	fmt.Printf("%sBlock size: %d\n", prefeix, h.Size)
+
 }
 
 type FullBox struct {
@@ -109,15 +118,59 @@ func (t TrackHeaderv1) Print() {
 	fmt.Printf("Height: %.2f\n", actualHeight)
 }
 
-type HandlerReference struct{
-	Predef [4]byte
+type HandlerReference struct {
+	Predef      [4]byte
 	HandlerType [4]byte
 }
 
-func (h HandlerReference) GetHandlerType() string{
+func (h HandlerReference) GetHandlerType() string {
 	return string(h.HandlerType[:])
 }
 
-func (h HandlerReference) Print(){
-	fmt.Printf("The track is of type: %s\n",h.GetHandlerType())
+func (h HandlerReference) Print() {
+	fmt.Printf("The track is of type: %s\n", h.GetHandlerType())
+}
+
+type MediaHeaderv0 struct{
+	CreationTime uint32
+	ModificationTime uint32
+	Timescale uint32
+	Duration uint32
+	Language uint16
+	Pre_defined uint16
+}
+
+func (m MediaHeaderv0) LanguageCode() string {
+    c1 := byte((m.Language>>10)&0x1F) + 0x60
+    c2 := byte((m.Language>>5)&0x1F) + 0x60
+    c3 := byte(m.Language&0x1F) + 0x60
+
+    return string([]byte{c1, c2, c3})
+}
+
+func (m MediaHeaderv0) Print(){
+	fmt.Printf("The track duration in second: %.2f\n",float64(m.Duration)/float64(m.Timescale))
+	fmt.Printf("The language code is: %s\n",m.LanguageCode())
+}
+
+type MediaHeaderv1 struct{
+	CreationTime uint64
+	ModificationTime uint64
+	Timescale uint32
+	Duration uint64
+	Language uint16
+	Pre_defined uint16
+}
+
+func (m MediaHeaderv1) LanguageCode() string {
+    c1 := byte((m.Language>>10)&0x1F) + 0x60
+    c2 := byte((m.Language>>5)&0x1F) + 0x60
+    c3 := byte(m.Language&0x1F) + 0x60
+
+    return string([]byte{c1, c2, c3})
+}
+
+func (m MediaHeaderv1) Print(){
+	fmt.Printf("The track duration in second: %.2f\n",float64(m.Duration)/float64(m.Timescale))
+	fmt.Printf("The language code is: %s\n",m.LanguageCode())
 }
